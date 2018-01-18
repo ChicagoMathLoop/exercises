3dcd3c70ff68958a42ff183f69447c521a79a9c0
Jared Davis <jared@haiq.us>
2018-01-18 13:36:11 -0600

    setup junior exercise
    incorporates test writing as a criterai

--- language/ruby/spec/scrabble/score_dictionary/file_reader_spec.rb
+++ language/ruby/spec/scrabble/score_dictionary/file_reader_spec.rb
@@ -1,19 +1,5 @@
 # frozen_string_literal: true
 
-RSpec.xdescribe Scrabble::ScoreDictionary::FileReader do
-  subject { described_class.new(default_example_file) }
-
-  describe '#contents' do
-    it 'loads the contents of the given file as a Hash' do
-      expect(subject.contents).to include(
-        1  => %w[A E I O U L N R S T],
-        2  => %w[D G],
-        3  => %w[B C M P],
-        4  => %w[F H V W Y],
-        5  => %w[K],
-        8  => %w[J X],
-        10 => %w[Q Z]
-      )
-    end
-  end
+RSpec.describe Scrabble::ScoreDictionary::FileReader do
+  describe '#contents'
 end
--- language/ruby/spec/scrabble/score_dictionary_spec.rb
+++ language/ruby/spec/scrabble/score_dictionary_spec.rb
@@ -1,40 +1,5 @@
 # frozen_string_literal: true
 
-RSpec.xdescribe Scrabble::ScoreDictionary do
-  subject { described_class }
-
-  describe '.build_from_legacy' do
-    it 'outputs the standard Hash' do
-      result = subject.build_from_legacy(default_example_file)
-
-      expect(result).to include(
-        'A' => 1,
-        'B' => 3,
-        'C' => 3,
-        'D' => 2,
-        'E' => 1,
-        'F' => 4,
-        'G' => 2,
-        'H' => 4,
-        'I' => 1,
-        'J' => 8,
-        'K' => 5,
-        'L' => 1,
-        'M' => 3,
-        'N' => 1,
-        'O' => 1,
-        'P' => 3,
-        'Q' => 10,
-        'R' => 1,
-        'S' => 1,
-        'T' => 1,
-        'U' => 1,
-        'V' => 4,
-        'W' => 4,
-        'X' => 8,
-        'Y' => 4,
-        'Z' => 10
-      )
-    end
-  end
+RSpec.describe Scrabble::ScoreDictionary do
+  describe '.build_from_legacy'
 end
